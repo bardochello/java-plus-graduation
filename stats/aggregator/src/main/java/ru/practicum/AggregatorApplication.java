035463d66cd201ea0e9908d60f44993508fdc023
Apti <aptigardanov@ya.ru>
2026-05-13 23:36:52 +0300

    recomendations 30

--- stats/aggregator/src/main/java/ru/practicum/AggregatorApplication.java
+++ stats/aggregator/src/main/java/ru/practicum/AggregatorApplication.java
@@ -1,9 +1,11 @@
 package ru.practicum;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@SpringBootApplication
 @EnableDiscoveryClient
 @ConfigurationPropertiesScan
 public class AggregatorApplication {
