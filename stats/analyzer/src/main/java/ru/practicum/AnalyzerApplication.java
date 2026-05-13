7aed8435216367c35c757de5ecf6e5cb5e56c3e3
Apti <aptigardanov@ya.ru>
2026-05-13 20:51:22 +0300

    reccomendations 25

--- stats/analyzer/src/main/java/ru/practicum/AnalyzerApplication.java
+++ stats/analyzer/src/main/java/ru/practicum/AnalyzerApplication.java
@@ -2,10 +2,11 @@ package ru.practicum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {KafkaAutoConfiguration.class})
 @EnableDiscoveryClient
 @EnableFeignClients
 public class AnalyzerApplication {
--- stats/collector/src/main/java/ru/practicum/CollectorApplication.java
+++ stats/collector/src/main/java/ru/practicum/CollectorApplication.java
@@ -2,10 +2,11 @@ package ru.practicum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {KafkaAutoConfiguration.class})
 @EnableDiscoveryClient
 @ConfigurationPropertiesScan
 public class CollectorApplication {
