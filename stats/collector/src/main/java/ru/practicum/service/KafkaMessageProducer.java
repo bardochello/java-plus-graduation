5f14b8da19bfe0d146c28c7a1a22878379ce53b5
Apti <aptigardanov@ya.ru>
2026-05-14 00:48:48 +0300

    recomendations 32

--- stats/collector/src/main/java/ru/practicum/service/KafkaMessageProducer.java
+++ stats/collector/src/main/java/ru/practicum/service/KafkaMessageProducer.java
@@ -1,20 +1,32 @@
 package ru.practicum.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
-import ru.practicum.config.KafkaProperties;
 import ru.practicum.ewm.stats.avro.UserActionAvro;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class KafkaMessageProducer implements MessageProducer {
 
     private final KafkaTemplate<String, UserActionAvro> kafkaTemplate;
-    private final KafkaProperties properties;
+
+    @Value("${collector.kafka.topic}")
+    private String topic;
 
     @Override
     public void sendUserAction(UserActionAvro userActionAvro) {
-        kafkaTemplate.send(properties.getUserActionsTopic(), userActionAvro);
+        kafkaTemplate.send(topic, userActionAvro)
+                .whenComplete((result, ex) -> {
+                    if (ex != null) {
+                        log.error("Ошибка отправки в Kafka: {}", ex.getMessage(), ex);
+                    } else {
+                        log.info("Отправлено в топик {}, offset={}",
+                                topic, result.getRecordMetadata().offset());
+                    }
+                });
     }
 }
