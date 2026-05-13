e3c1f3c2f96727cb416c54ffe291f59bc9f45fbd
Apti <aptigardanov@ya.ru>
2026-05-13 05:26:14 +0300

    recommendations #7

---
+++ stats/collector/src/main/java/ru/practicum/collector/config/GrpcPortEurekaRegistrar.java
@@ -0,0 +1,28 @@
+package ru.practicum.collector.config;
+
+import com.netflix.appinfo.ApplicationInfoManager;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import net.devh.boot.grpc.server.event.GrpcServerStartedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class GrpcPortEurekaRegistrar {
+
+    private final ApplicationInfoManager applicationInfoManager;
+
+    @EventListener
+    public void onGrpcServerStarted(GrpcServerStartedEvent event) {
+        int grpcPort = event.getServer().getPort();
+        log.info("gRPC server started on port {}, registering in Eureka metadata", grpcPort);
+
+        applicationInfoManager.getInfo()
+                .getMetadata()
+                .put("gRPC.port", String.valueOf(grpcPort));
+
+        log.info("Registered gRPC.port={} in Eureka metadata", grpcPort);
+    }
+}
