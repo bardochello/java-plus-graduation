f112d6b01304729b0bcbba3154f0950fb00a2166
Apti <aptigardanov@ya.ru>
2026-05-13 23:12:24 +0300

    reccomendations 27

--- stats/analyzer/src/main/java/ru/practicum/AnalyzerApplication.java
+++ stats/analyzer/src/main/java/ru/practicum/AnalyzerApplication.java
@@ -1,12 +1,10 @@
 package ru.practicum;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication(exclude = {KafkaAutoConfiguration.class})
+
 @EnableDiscoveryClient
 @EnableFeignClients
 public class AnalyzerApplication {
--- stats/collector/src/main/java/ru/practicum/CollectorApplication.java
+++ stats/collector/src/main/java/ru/practicum/CollectorApplication.java
@@ -1,12 +1,9 @@
 package ru.practicum;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication(exclude = {KafkaAutoConfiguration.class})
 @EnableDiscoveryClient
 @ConfigurationPropertiesScan
 public class CollectorApplication {
